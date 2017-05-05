e976b40003b54a547cc255c882b506e647782e0f
David Schuster <searay330@imageknight.com>
2017-05-05 00:12:06 -0400

    added files and algos

---
+++ files and algos/src/FileAlgo/ReadandWrite.java
@@ -0,0 +1,25 @@
+package FileAlgo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ReadandWrite {
+
+	public static void main(String args[]) throws IOException {
+		int min = Integer.MAX_VALUE;
+		ArrayList<String> FileLines = iNeed.returnArray("src/FileAlgo/input.txt");
+		ArrayList<Integer> Converted = new ArrayList<Integer>();
+		for (int i = 0; i < FileLines.size(); i++) {
+			Converted.add(Integer.parseInt(FileLines.get(i)));
+		}
+		Collections.sort(Converted);
+		for (int i = 0; i < Converted.size(); i++) {
+			if (min > Converted.get(i)) {
+				min = Converted.get(i);
+			}
+		}
+		iNeed.writeMaxInt(min, "src/FileAlgo/output.txt");
+	}
+
+}
---
+++ files and algos/src/FileAlgo/iNeed.java
@@ -0,0 +1,24 @@
+package FileAlgo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class iNeed {
+
+	public static ArrayList<String> returnArray(String s) throws IOException {
+		ArrayList<String> result = new ArrayList<String>();
+		for (String line : Files.readAllLines(Paths.get(s))) {
+			result.add(line);
+		}
+		return result;
+	}
+
+	public static void writeMaxInt(int i, String s) throws IOException {
+		String result = i + "";
+		Files.write(Paths.get(s), result.getBytes());
+
+	}
+
+}
