f50fdf50115259defb5625bd9dd428f6ffe62272
Richard Hanulewicz <rshanule@buffalo.edu>
2017-05-05 12:38:24 -0400

    Made it a little better

--- code reading/src/funExample.java
+++ code reading/src/funExample.java
@@ -3,10 +3,8 @@ public class funExample {
 	public static void main(String[] args) {
 		String str = " ";
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 111)
 				str = (char)i + str;
-
 		}
 		for (int i = 0; i < 256; i++) {
 			if (i == 108) {
@@ -15,45 +13,32 @@ public class funExample {
 			}
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 101)
 				str = (char) i + str;
-
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 72)
 				str = (char) i + str;
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 87)
 				str += (char) i;
-
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 111)
 				str += (char) i;
-
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 114)
 				str += (char) i;
-
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 108)
 				str += (char) i;
-
 		}
 		for (int i = 0; i < 256; i++) {
-
 			if (i == 100)
 				str += (char) i;
-
 		}
 		System.out.println(str);
 	}
